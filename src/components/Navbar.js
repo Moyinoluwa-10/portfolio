463c3d819299c89b5f31ab4849272defd9f97c57
Adelowo Moyinoluwa <moyinadelowo@gmail.com>
2022-08-13 20:08:09 +0100

    Added Navbar Section

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    
+        <div className='navbar'>
+
+            <div className='name-tag'><h1>Moyinoluwa Adelowo</h1></div>
+
+            <nav>
+                <ul>
+                    <li><a href='#home' className='nav-link'>Home</a></li>
+                    <li><a href='#about' className='nav-link'>About Me</a></li>
+                    <li><a href='#contact' className='nav-link'>Contact</a></li>
+                    <li><a href='#project' className='nav-link'>Projects</a></li>
+                </ul>
+            </nav> 
+        
+        </div>
+    
+    </>
+  )
+}
+
+export default Navbar
