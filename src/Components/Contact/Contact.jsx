8ffa9b841a85f495803d95e80d1f9c14a085dfff
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-04-16 06:57:00 +0100

    fix: fixed form submitting bug

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Contact.css";
-import PageLoader from "../Pageloader/PageLoader";
-
 import { useFormik } from "formik";
 import toast from "react-hot-toast";
-
 import Aos from "aos";
-import "aos/dist/aos.css";
 
 Aos.init({ duration: 1500 });
 
 const Contact = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   const validate = (values) => {
     const errors = {};
 
@@ -22,7 +16,8 @@ const Contact = () => {
       values.name.split(" ").length < 2 ||
       values.name.split[1] === " "
     ) {
-      errors.name = "Please fill out this field in the proper format";
+      errors.name =
+        'Please fill out this field in the format "Firstname Lastname"';
     }
 
     if (!values.email) {
@@ -47,10 +42,9 @@ const Contact = () => {
       message: "",
     },
     validate,
-    onSubmit: (values) => {
-      setIsLoading(true);
+    onSubmit: (values, { resetForm }) => {
       const toastId = toast.loading("Loading...");
-      const url = "https://mailer-lake.vercel.app/api/v1/send-emai";
+      const url = "https://mailer-lake.vercel.app/api/v1/send-email";
       fetch(url, {
         method: "POST",
         headers: {
@@ -60,38 +54,23 @@ const Contact = () => {
       })
         .then((response) => response.json())
         .then((result) => {
-          console.log(result);
+          // console.log(result);
           if (result.status === true) {
-            // values.name = "";
-            // values.email = "";
-            // values.message = "";
-            setIsLoading(false);
-            toast.dismiss(toastId);
-            toast.dismiss();
-            toast.success(
-              JSON.stringify("Message sent successfully", {
-                id: toastId,
-              })
-            );
+            resetForm({ values: "" });
+            values = formik.initialValues;
+            toast.success("Message sent successfully", {
+              id: toastId,
+            });
           } else {
-            toast.dismiss(toastId);
-            toast.dismiss();
-            toast.error(
-              JSON.stringify("Message Failed to Send", {
-                id: toastId,
-              })
-            );
-            // toast.error(JSON.stringify("Message Failed to Send", null, 2));
+            toast.error("An error occured", {
+              id: toastId,
+            });
           }
         })
         .catch((err) => {
-          setIsLoading(false);
-          toast.dismiss();
-          toast.error(
-            JSON.stringify("An error occured", {
-              id: toastId,
-            })
-          );
+          toast.error("An error occured", {
+            id: toastId,
+          });
           console.log(err);
         });
     },
@@ -99,80 +78,81 @@ const Contact = () => {
 
   return (
     <div className="contact" id="contact">
-      {isLoading && <PageLoader />}
       <h1 className="contact__heading" data-aos="zoom-in">
         Contact Me
       </h1>
-      <form onSubmit={formik.handleSubmit} data-aos="zoom-in">
+
+      <form className="form" onSubmit={formik.handleSubmit} data-aos="zoom-in">
         <div className="feedback-title">
-          <p>Need a Service?</p>
+          <p>Need a service, want to collaborate or just want to say hello?</p>
           <h3>Send A Message</h3>
         </div>
-        <div className="form-control">
-          <input
-            type="text"
-            name="name"
-            required
-            placeholder="Full Name"
-            className="input"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            onBlur={formik.handleBlur}
-          />
+        <div className="form-group">
+          <div className="form-control">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              placeholder="Full Name"
+              className="input"
+              {...formik.getFieldProps("name")}
+            />
+            <label htmlFor="name" className="label">
+              Full Name
+            </label>
+          </div>
           {formik.touched.name && formik.errors.name ? (
             <div className="error">{formik.errors.name}</div>
           ) : null}
         </div>
-        <div className="form-control">
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="Email Address"
-            className="input"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            onBlur={formik.handleBlur}
-          />
+
+        <div className="form-group">
+          <div className="form-control">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="Email Address"
+              className="input"
+              {...formik.getFieldProps("email")}
+            />
+            <label htmlFor="email" className="label">
+              Email Address
+            </label>
+          </div>
           {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
           ) : null}
         </div>
-        <div className="form-control">
-          <textarea
-            name="message"
-            id=""
-            cols="30"
-            rows="5"
-            required
-            placeholder="Message"
-            className="textarea"
-            onChange={formik.handleChange}
-            value={formik.values.message}
-            onBlur={formik.handleBlur}
-          ></textarea>
+
+        <div className="form-group">
+          <div className="form-control">
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="5"
+              required
+              placeholder="Message"
+              className="textarea"
+              {...formik.getFieldProps("message")}
+            ></textarea>
+            <label htmlFor="message" className="label">
+              Message
+            </label>
+          </div>
           {formik.touched.message && formik.errors.message ? (
             <div className="error">{formik.errors.message}</div>
           ) : null}
         </div>
-        <div className="form-control">
+
+        <div className="form-group">
           <button type="submit" className="btn">
-            Send
+            <p>Send</p>
           </button>
         </div>
-
-        {/* <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        /> */}
       </form>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
+import "aos/dist/aos.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
