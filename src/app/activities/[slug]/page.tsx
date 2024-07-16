196a155dd4d308b99e8e291151299602909d0c99
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-07-16 03:27:11 -0700

    feat: created activity information page

---
+++ src/app/activities/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import ActivityInfo from "@/components/activities/activities/info";
+
+const page = ({ params }: { params: { slug: string } }) => {
+  return <ActivityInfo />;
+};
+
+export default page;
