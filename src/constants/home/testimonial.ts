154ed660b0f69bb8890dacd2ead3d63f7a4d07ec
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-09-27 12:11:24 -0500

    feat: added testimonials

--- src/constants/home/testimonial.ts
+++ src/constants/home/testimonial.ts
@@ -1,25 +1,27 @@
-import certOne from "@/assets/images/contact/contact.png";
+import magnus from "@/assets/images/home/magnus-egerstedt.png";
+import samuel from "@/assets/images/home/samuel-adewuyi.jpg";
+import ed from "@/assets/images/home/ed-pearson.jpg";
 
 export const TESTIMONIALS = [
   {
-    name: "John Doe",
-    title: "CEO of Company",
+    name: "Magnus Egerstedt",
+    title: "Stacey Nicholas Dean of Engineering",
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: certOne,
+      "I had the pleasure of hosting Moyinoluwa Adelowo at the UCI Robot Ecology Lab during the summer of 2024. Moyinoluwa was a hard-working and highly capable team member, consistently demonstrating a strong work ethic, technical expertise, and a collaborative spirit. Moyinoluwa worked on integrating new robots into our swarm robotics testbed, the Robotarium. His ability to quickly learn new skills, take on complex tasks, and work effectively with the team made him an effective member of the lab in a short amount of time. I'm confident that his skills and proactive approach will make him a strong addition to any team or organization.",
+    image: magnus,
   },
   {
-    name: "Jane Doe",
-    title: "CTO of Company",
+    name: "Samuel Adewuyi",
+    title: "Founder, Stellar Eminence",
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: certOne,
+      "I highly recommend Moyinoluwa, who has been an exceptional Frontend Team Lead at Stellar Eminence. His expertise in Software development, combined with his leadership and problem-solving skills, has significantly contributed to the success of our projects. Any company or team would greatly benefit from his technical abilities and dedication to delivering top-notch results.",
+    image: samuel,
   },
   {
-    name: "John Smith",
-    title: "COO of Company",
+    name: "Ed Pearson",
+    title: "Assistant Professor, Alabama A&M University",
     quote:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: certOne,
+    image: ed,
   },
 ];
