1276d7c27fcfc19c5faf2d89d65f3f25c5ab7012
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-09-28 21:09:01 -0500

    style: hero component update]

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -2,7 +2,7 @@ import Buttons from "./buttons";
 
 const Hero = () => {
   return (
-    <div className="min-h-[calc(100vh-70px)] md:min-h-[800px] md:h-screen md:max-h-[850px] dark:bg-black dark:text-primary-100 px-5 font-semibold flex flex-col">
+    <div className="min-h-[calc(100vh-70px)] dark:bg-black dark:text-primary-100 px-5 py-10 font-semibold flex flex-col">
       <div className="container grid items-center flex-grow">
         <div>
           <h1 className="text-[86px] md:text-[144px] lg:text-[250px] leading-none text-center mb-20">
